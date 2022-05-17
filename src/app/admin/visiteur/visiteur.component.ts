8e3e1c85538cfc4da91866072a3f269eb002a125
amira <fkiriamira814@gmail.com>
2022-05-17 17:36:22 +0200

    test

--- src/app/admin/visiteur/visiteur.component.ts
+++ src/app/admin/visiteur/visiteur.component.ts
@@ -2,17 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Toast } from 'src/app/shared/services/toast.service';
 import { VisiteurService } from './visiteur.service';
+import { ConfirmationService } from 'primeng/api';
+import { DatePipe } from '@angular/common';
+import { pdfservice } from 'src/app/shared/services/pdfservice';
 
 @Component({
   selector: 'app-visiteur',
   templateUrl: './visiteur.component.html',
-  styleUrls: ['./visiteur.component.css']
+  styleUrls: ['./visiteur.component.css'],
+  providers: [ConfirmationService, DatePipe, pdfservice],
+
 })
 export class VisiteurComponent implements OnInit {
 allvisiteur : any ;
 loading: boolean = false;
+visiteur: { id: string; archived: boolean };
 
-  constructor(private Visiteurservice : VisiteurService,private toast: Toast,private router: Router) { }
+  constructor(private Visiteurservice : VisiteurService, 
+    private ConfirmationService: ConfirmationService,
+    private toast: Toast,private router: Router) { }
 
   ngOnInit(): void {
     this.getallvisiteur()
@@ -27,6 +35,17 @@ this.loading = false;
 console.log(this.allvisiteur)
   })
 }
+
+DeleteClientModal = ({ id, archived }) => {
+  this.ConfirmationService.confirm({
+    message: "Voulez-vous supprimer cet visiteur?",
+    header: "Confirmation",
+    icon: "pi pi-info-circle",
+  });
+  // client to be archived
+  this.visiteur = { id, archived };
+};
+
 update(id){
   this.Visiteurservice.activer(id).subscribe((res)=>{
     console.log(res)
@@ -38,5 +57,18 @@ update(id){
 
   })
 }
+supprimer(){
+  this.Visiteurservice.delete(this.visiteur.id).subscribe((res)=>{
+    console.log(res)
 
+    this.toast.success("La suppression  de compte est avec succès .");
+    this.router.navigate(["admin/visiteur"]);
+
+    location.reload();
+
+  })
+}
+cancel = () => {
+  this.ConfirmationService.close();
+};
 }
